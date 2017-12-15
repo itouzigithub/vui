a2b835bdee86356ebef1f43f510a008e14bf0aa3
bison1994 <942915554@qq.com>
2017-12-15 14:25:35 +0800

    -

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
+// directives
+import { 
+	increase,
+	translateX,
+	translateY
+} from './lib/directives'
+
+// filters
+import { 
+	addThousandSeparator,
+	addSpace,
+	hideNumber,
+	formatDate,
+	formatTime,
+	formatDateTime
+} from './lib/filters'
+
 // utils
-import { increase } from './lib/directives'
-import { addThousandSeparator } from './lib/filters'
+import imgGenerator from './lib/utils/img-generator.js'
 
 // widgets
-import banner from './lib/widget/banner/banner.vue'
+import banner from './lib/widgets/banner/banner.vue'
+import cascade from './lib/widgets/cascade/cascade-picker.vue'
+import iptPhone from './lib/widgets/input/ipt-phone.vue'
+import iptNumber from './lib/widgets/input/ipt-number.vue'
+import iptPassword from './lib/widgets/input/ipt-password.vue'
+import iptSearch from './lib/widgets/input/ipt-search.vue'
 
 module.exports = {
+	// directives
 	increase,
+	translateX, 
+	translateY,
+  // filters
 	addThousandSeparator,
-	banner
+	addSpace,
+	hideNumber,
+	formatDate,
+	formatTime,
+	formatDateTime,
+	// utils
+	imgGenerator,
+  // widgets
+	banner,
+	cascade,
+	iptPhone,
+	iptNumber,
+	iptPassword,
+	iptSearch
 }
